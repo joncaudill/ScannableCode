ff33546d8d485261c82d0fa32a731764acbd3168
joncaudill <50488018+joncaudill@users.noreply.github.com>
2025-06-28 12:33:26 -0400

    Add project files.

---
+++ ScannableCode/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using ZXing;
+
+namespace ScannableCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check if a string argument is provided
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please provide a string to encode as a command line argument.");
+                return;
+            }
+
+            string strBarcodeString = args[0];
+
+            var barcodeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.CODE_128,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = 400,
+                    Height = 125,
+                    Margin = 13
+                }
+            };
+
+            Bitmap barcodeBitmap = barcodeWriter.Write(strBarcodeString);
+
+            string outputFilePath = "barcode.png";
+            barcodeBitmap.Save(outputFilePath, System.Drawing.Imaging.ImageFormat.Png);
+            Console.WriteLine($"Barcode saved to {outputFilePath}");
+        }
+    }
+}
